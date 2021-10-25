5852bbd524121d146ff0c1123e75576e2cb5d0fd
pritioktaviani <pritioktaviani09@gmail.com>
2021-10-25 13:07:38 +0700

    commit 4

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -10,17 +10,22 @@ class CobaController extends Controller
     
     public function index()
     {
-        $friends = Friends::paginate(3);
+        $friends = Friends::orderBy('id' , 'desc')->paginate(3);
     
-        return view('index', compact('friends'));
+        return view('friends.index', compact('friends'));
     }
     public function create()
     {
-        return view('create',);
+        return view('friends.create',);
     }
     public function store(Request $request)
     {
         // Validate the request...
+        $request->validate([
+            'nama' => 'required|unique:friends|max:255',
+            'no_tlp' => 'required|numeric',
+            'alamat' => 'nullable',
+        ]);
 
         $friends = new Friends;
 
@@ -29,5 +34,35 @@ class CobaController extends Controller
         $friends->alamat = $request->alamat;
 
         $friends->save();
+
+        return redirect('/');
+    }
+    public function show($id)
+    {
+        $request->validate([
+            'nama' => 'required|unique:friends|max:255',
+            'no_tlp' => 'required|numeric',
+            'alamat' => 'nullable',
+        ]);
+        $friend = Friends::where('id' , $id)->first();
+        return view('friends.show', ['friend'=> $friend]);
+    }  
+    public function edit($id)
+    {
+        $friend = Friends::where('id' , $id)->first();
+        return view('friends.edit', ['friend'=> $friend]);
+    }
+    public function update(Request $request, $id)
+    {
+        Friends::find($id)->update([
+            'nama' => $request->nama,
+            'no_tlp' => $request->no_tlp,
+            'alamat' => $request->alamat
+        ]);
+        return redirect('/');
+    }
+    public function destroy($id){
+        Friends::find($id)->delete();
+        return redirect('/');
     }
 }
--- resources/views/Friends/index.blade.php
+++ resources/views/Friends/index.blade.php
@@ -8,11 +8,14 @@
 
 <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">{{ $friend['nama']}}</h5>
+      <a href="/friends/{{$friend['id']}}" class="card-title">{{ $friend['nama']}}</a>
       <h6 class="card-subtitle mb-2 text-muted">{{ $friend['no_tlp'] }}</h6>
       <p class="card-text">{{ $friend['alamat'] }}</p>
-      <a href="#" class="card-link btn-warning">Edit teman</a>
-      <a href="#" class="card-link btn-danger">Delete teman</a>
+      <a href="/friends/{{$friend['id']}}/edit" class="card-link btn-warning">Edit teman</a>
+      <form action="/friends/{{$friend['id']}}" method="POST">
+        @csrf
+        @method('DELETE')
+      <button class="card-link btn-danger">Delete teman</button>
     </div>
   </div>
   
